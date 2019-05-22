42a683e2c2f5536b6c990a010ebe1a6266c148f2
Lgoldfinch <louis.goldfinch@academytrainee.com>
2019-05-22 11:54:51 +0100

    init commit

---
+++ Assets/PlayerDragHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PlayerDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        image.color = Color.black;
+    }
+}
