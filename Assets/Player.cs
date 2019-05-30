c5030996038e7ea8047472c0cbbf26deacff030b
Lgoldfinch <louis.goldfinch@academytrainee.com>
2019-05-30 12:38:15 +0100

    Implemented zoom on the camera

---
+++ Assets/Player.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : ScriptableObject
+{
+    public Transform playerPosition;
+    public Text playerNumber;
+    public Sprite icon = null; // does this need to be null?
+}
---
+++ Assets/ZoomScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomScript : MonoBehaviour
+{
+
+    public float zoomSize = 110;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (zoomSize > 2)
+            {
+                zoomSize -= 1;
+            }
+        }
+
+       else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (zoomSize < 110)
+            { 
+                zoomSize += 1;
+            }
+
+        }
+
+        GetComponent<Camera>().orthographicSize = zoomSize;
+
+    }
+}
