93ae32b123243b33c92d1f9c11ca9e9190705a5f
Lgoldfinch <louis.goldfinch@academytrainee.com>
2019-05-31 12:15:17 +0100

    Enabled wasd navigation of the game

--- Assets/CameraPanning.cs
+++ Assets/CameraPanning.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraPanning : MonoBehaviour
 {
-    Vector2 cameraPosition;
+    Vector3 cameraPosition;
 
     void Update()
     {
@@ -12,27 +12,28 @@ public class CameraPanning : MonoBehaviour
 
         if (Input.GetKeyDown("a") || Input.GetKey(KeyCode.LeftArrow))
             {
-                cameraPosition.x = cameraPosition.x - 5;
+            cameraPosition.x = cameraPosition.x - 5f;
             Debug.Log("left");
             }
 
             if (Input.GetKeyDown("w") || Input.GetKey(KeyCode.UpArrow))
             {
-            cameraPosition[1] = cameraPosition.y + 5;
+            cameraPosition.y = cameraPosition.y + 5f;
+
             Debug.Log("up");
 
         }
         if (Input.GetKeyDown("s") || Input.GetKey(KeyCode.DownArrow))
             {
-            cameraPosition[1] = cameraPosition.y - 5;
+            cameraPosition.y = cameraPosition.y - 5f;
             Debug.Log("down");
 
         }
         if (Input.GetKeyDown("d") || Input.GetKey(KeyCode.RightArrow))
             {
-            cameraPosition[0] = cameraPosition.x + 5;
+            cameraPosition.x = cameraPosition.x + 5f;
             Debug.Log("right");
-
         }
+        transform.position = cameraPosition;
     }
 }
