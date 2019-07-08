3d88c65af21531cc5ebaa46f648f7558915d065d
Lgoldfinch <louis.goldfinch@academytrainee.com>
2019-07-08 22:45:21 +0100

    Another attempt at adding line functionality

---
+++ Assets/LineCreator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour
+{
+    public GameObject linePrefab;
+
+    Line activeLine;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GameObject lineGo = Instantiate(linePrefab);
+            activeLine = lineGo.GetComponent<Line>();
+        }
+
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        activeLine.UpdateLine(mousePos);
+    }
+}
