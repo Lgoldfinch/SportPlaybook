cb6adea2ae9d6b167ca1dcf4a1a90b6d19ce61d3
Lgoldfinch <louis.goldfinch@academytrainee.com>
2019-07-22 20:05:52 +0100

    Bringing over missing functionality inc. player drag and drop.

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+   public void ChangeToScene(int sceneToChangeTo) 
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+}
---
+++ Assets/PlayerDragHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.localPosition = Vector2.zero;
+    }
+}
