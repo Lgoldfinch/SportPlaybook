b2445efa06b752646a28bf940cfb4062357ba932
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2019-08-12 17:53:54 +0100

    WIP, adding btn to replace double click

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,8 +4,5 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    public Transform playerPosition;
-    public Text playerNumber;
-    public Sprite icon = null; // does this need to be null?
-
+    
 }
