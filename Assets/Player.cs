18c93556b4e411863c6c7d7ea90bc07b58287416
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2019-09-18 17:37:20 +0100

    asd

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,5 +4,15 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    
+    public bool isPlayerclicked;
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0)) isPlayerclicked = true; 
+    }
+
+    private void OnMouseUp()
+    {
+        if (Input.GetMouseButtonUp(1)) isPlayerclicked = false;
+    }
 }
