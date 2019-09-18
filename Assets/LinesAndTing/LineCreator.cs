8fda34314ae515a679a78949c5e1c9e0c9bf6e18
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2019-09-18 17:15:12 +0100

    Implemented right click line creation

--- Assets/LinesAndTing/LineCreator.cs
+++ Assets/LinesAndTing/LineCreator.cs
@@ -10,36 +10,21 @@ public class LineCreator : MonoBehaviour {
     private ChangeTextOfButton changeTextScript;
     public bool isPlayerDraggable = true;
 
-  void Update()
+    void Update()
     {
-        if (Input.GetMouseButtonDown(0)) DoubleClickLineCreation();
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            MakeLine();
+        }
 
-        if (Input.GetMouseButtonUp(0)) activeLine = null;
-        
+        if (Input.GetMouseButtonUp(1)) activeLine = null;
+
 
         if (activeLine != null)
         {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             activeLine.UpdateLine(mousePos);
         }
-    }
-  
-    private void DoubleClickLineCreation()
-    {
-        isPlayerDraggable = true;
-        clicked++;
-        if (clicked == 1) clickTime = Time.time; // initial click 
-
-        if (clicked > 1 && Time.time - clickTime < clickDelay) // double click 
-        {
-            clicked = 0;
-            clickTime = 0;
-            MakeLine();
-            isPlayerDraggable = false; 
-
-        }
-        else if (clicked > 2 || Time.time - clickTime > 1) clicked = 0; // too long
     }
   
     private void MakeLine()
