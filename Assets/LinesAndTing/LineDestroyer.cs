b61f1739ff5f5d4c99f717edc9d1acef2de5e54f
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2019-09-21 13:28:36 +0100

    Line deletes when player is moved

--- Assets/LinesAndTing/LineDestroyer.cs
+++ Assets/LinesAndTing/LineDestroyer.cs
@@ -3,17 +3,15 @@
 public class LineDestroyer : MonoBehaviour
 {
     public Transform playerTransform;
-
     private void Update()
     {
-        DeleteLine();
+        DeleteLine(false);
     }
 
-    private void DeleteLine()
+    public void DeleteLine(bool playerMoved)
     {
-        if (playerTransform.childCount > 2)
-        {
-            Destroy(GetComponent<Transform>().GetChild(1).gameObject);
-        }
+        if (playerTransform.childCount > 2) Destroy(GetComponent<Transform>().GetChild(1).gameObject);
+
+        if (playerMoved && playerTransform.childCount == 2) Destroy(GetComponent<Transform>().GetChild(1).gameObject);
     }
 }
