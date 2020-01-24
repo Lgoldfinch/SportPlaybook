dfc8548c578cdc527c1a5f77bbec0ec8b6df11dd
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2020-01-24 16:54:10 +0000

    Made changes to EventInformation

--- Assets/Events/EventInformation.cs
+++ Assets/Events/EventInformation.cs
@@ -4,4 +4,7 @@ public abstract class EventInformation : MonoBehaviour
 {
     public EventTypeHandler.DynamicEventTypes eventType;
     public bool hasEventBeenMade;
+    public Vector2 eventOriginPosition = Vector2.zero;
+    public Vector2 eventEndPosition = Vector2.zero;
+
 }
